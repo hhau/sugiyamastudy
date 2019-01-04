33065c33a0b4284b046f829e9aef8f692576ce81
Andrew Manderson <a.manderson@live.co.uk>
2019-01-04 10:47:16 +0000

    notes for future me

--- R/sugiyamastudy.R
+++ R/sugiyamastudy.R
@@ -9,7 +9,7 @@ NULL
 # some globals that I might adjust
 
 # how many standard deviations to go beyond the min/max of a sample
-# (for plotting)
+# (for plotting) - this really does not work well for cauchy distributions (duh)
 SD_FACTOR = 0.5
 PLOT_HEIGHT = 6
 PLOT_WIDTH = 9

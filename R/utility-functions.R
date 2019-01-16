2c799df7acf36f2e979d037aff05ca010deb2c45
Andrew Manderson <a.manderson@live.co.uk>
2019-01-16 15:24:13 +0000

    added utility function for factorial design like simulation studies - useful to avoid many nested for/lapply calls (does call expand.grid though so careful there)

--- R/utility-functions.R
+++ R/utility-functions.R
@@ -7,7 +7,7 @@ get_plot_limits <- function(numerator_samples, denominator_samples) {
   nu_max <- max(numerator_samples) + SD_FACTOR * stats::sd(numerator_samples)
   de_max <- max(denominator_samples) + SD_FACTOR * stats::sd(denominator_samples)
   plot_max <- max(nu_max, de_max)  
-  
+
   return(list(
     min = plot_min,
     max = plot_max
@@ -45,3 +45,45 @@ make_file_name <- function(ratio_obj) {
   sep = "-")
   return(file_name_string)
 }
+
+# function is written to be called like:
+#
+# test_func <- function(alpha, beta) {
+#   alpha^2 + beta
+# }
+#
+# factorial_parallel_apply(test_func, alpha = 1:3, beta = 1:10)
+#
+# and this will compute all the output whilst parallelising over alpha
+factorial_parallel_apply <- function(f, ...) {
+  f_name <- deparse(substitute(f))
+  inputs <- list(...)
+
+  # parallelise over the first one
+  par_arg_name <- names(inputs)[1]
+  inner_inputs <- inputs[-1]
+  full_inputs <- expand.grid(inner_inputs) 
+  full_inputs[[f_name]] <- NA
+  
+  # expecting vector types
+  res_list <- parallel::mclapply(
+    X = inputs[[1]],
+    mc.cores = parallel::detectCores(),
+    FUN = function(outer_arg) {
+      # there is probably a better way of doing this (rather than looping)
+      for (ii in seq_len(nrow(full_inputs))) {
+        f_args <- as.list(full_inputs[ii, ])
+        f_args[[f_name]] <- NULL
+        f_args[[par_arg_name]] <- outer_arg
+        full_inputs[[f_name]][ii] <- do.call(f, f_args)
+      }
+      
+      full_inputs[[par_arg_name]] <- outer_arg
+      return(full_inputs)
+    }
+  )
+  
+  res <- dplyr::bind_rows(res_list)
+  return(res)
+}
+
